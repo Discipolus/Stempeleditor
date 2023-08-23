e110e76bc3097a1a2d85bd784090c495a5d71988
Discipolus <t.labinnah@web.de>
2023-08-23 10:16:51 +0200

    added views and Stempel structure

---
+++ MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,43 @@
+﻿using Stempeleditor.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stempeleditor.MVVM.ViewModel
+{
+    internal class MainViewModel : ObservableObject
+    {
+        public RelayCommand UebersichtViewCommand { get; set; }
+        public RelayCommand StempelEditierenViewCommand { get; set; }
+
+        public UebersichtViewModel UebersichtVM { get; set; }
+        public StempelEditierenViewModel StempelEditierenVM { get; set; }
+        
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set { _currentView = value;
+                OnPorpertyChanged();
+            }
+        }
+        public MainViewModel()
+        {
+            UebersichtVM = new UebersichtViewModel();
+            StempelEditierenVM = new StempelEditierenViewModel();
+            CurrentView = UebersichtVM;
+
+            UebersichtViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = UebersichtVM;
+            });
+            StempelEditierenViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = StempelEditierenVM;
+            });
+        }
+    }
+}
---
+++ MVVM/ViewModel/UebersichtViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Stempeleditor.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stempeleditor.MVVM.ViewModel
+{
+    internal class UebersichtViewModel : ObservableObject
+    {
+        public UebersichtViewModel()
+        {
+
+        }
+    }
+}
