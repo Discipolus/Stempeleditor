50c59d3c39305db7b1e024d14fc76f17664a2e39
Vincent Hannibal <Vincent.Hannibal@yomagroup.de>
2023-08-24 23:13:08 +0200

    24.08.2023

--- Engine/Elements/Stempel.cs
+++ Engine/Elements/Stempel.cs
@@ -1,11 +1,10 @@
-﻿using Engine.Elements;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Engine
+namespace Engine.Elements
 {
     internal class Stempel
     {
---
+++ Engine/Storage/Storage.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+
+namespace Engine.Storage
+{
+    public static class Storage
+    {
+        #region Properties
+        static string server = "";
+        static string database = "";
+        static string? user = "";
+        static string? password = "";
+        #endregion
+
+        public static void connect()
+        {
+
+            string connectionString = $"Server={server};Database={database};Authentication=Actice Directory Integrated;";
+
+            SqlConnection connection = new SqlConnection(connectionString);
+            try
+            {
+                connection.Open();
+                Console.WriteLine("Database connection opened successfully");
+            }
+            catch (Exception ex) 
+            {
+                Console.WriteLine("Failed in opening a Database connection via Active Directory authentification");
+                Console.WriteLine(ex.Message);
+                Console.Error.WriteLine(ex.ToString());
+                Console.WriteLine("Trying to connect via username and password");
+
+                try
+                {
+                    Console.WriteLine("username: ");
+                    user = Console.ReadLine();
+                    Console.WriteLine("password: ");
+                    password = Console.ReadLine();
+                    connectionString = $"Server={server};Database={database};Authentication=SqlPassword;User ID={user};Password={password}";
+                    connection = new SqlConnection(connectionString);
+
+                }
+                catch (Exception ex2)
+                {
+                    Console.WriteLine("Failed in opening a Database connection via username/password");
+                    Console.WriteLine(ex2.Message);
+                    Console.Error.WriteLine(ex2.ToString());
+                }
+
+            }
+        }
+    }
+}
