c4872006f14ef080ecdd6f93a834a2db8b5ef849
Vincent Hannibal <Vincent.Hannibal@yomagroup.de>
2023-12-21 12:41:18 +0100

    21.12.2023

--- SQLDatabaseConnection/DummyDatabase.cs
+++ SQLDatabaseConnection/DummyDatabase.cs
@@ -10,9 +10,14 @@ namespace SQLDatabaseConnection
     public class DummyDatabase : IStorageService
     {
         public List<string> ladeStempelListe()
-        {
-            string stempel = "<?xml version=\"1.0\" encoding=\"utf-8\"?><Stempelverfuegung><Id>f32de6ee-65cd-4197-bf6b-ad1e7a443dde</Id><Name>Beispielstempel</Name><ErstellInformationenAnzeigen>False</ErstellInformationenAnzeigen><Farbe>#FF0000FF</Farbe><AufgabeErzeugen>False</AufgabeErzeugen><Beschreibung><PlatzhalterRef>1</PlatzhalterRef><br />Ab am <ZeitstempelRef/></Beschreibung><PlatzhalterListe><Platzhalter><Id>1</Id><Name>Texteingabe</Name><Typ>TextBoxEinzeilig</Typ><Werte><Wert></Wert><Vorbelegung></Vorbelegung></Werte></Platzhalter></PlatzhalterListe></Stempelverfuegung>";
-            return new List<string> { stempel };            
+        {   
+            return new List<string> 
+            {
+                "<Stempelverfuegung><Id>00000000-0000-0000-0000-000000000001</Id><Name>teststempel1</Name><ErstellInformationenAnzeigen>False</ErstellInformationenAnzeigen><Farbe>#FF0000FF</Farbe><AufgabeErzeugen>False</AufgabeErzeugen><Beschreibung><PlatzhalterRef>1</PlatzhalterRef><br />Ab am <ZeitstempelRef /></Beschreibung><PlatzhalterListe><Platzhalter><Id>1</Id><Name>Texteingabe</Name><Typ>TextBoxEinzeilig</Typ><Werte><Wert /><Vorbelegung /></Werte></Platzhalter></PlatzhalterListe></Stempelverfuegung>",
+                "<Stempelverfuegung><Id>00000000-0000-0000-0000-000000000002</Id><Name>teststempel2</Name><ErstellInformationenAnzeigen>False</ErstellInformationenAnzeigen><Farbe>#FF0000FF</Farbe><AufgabeErzeugen>False</AufgabeErzeugen><Beschreibung><PlatzhalterRef>1</PlatzhalterRef><br />Ab am <ZeitstempelRef /></Beschreibung><PlatzhalterListe><Platzhalter><Id>1</Id><Name>Texteingabe</Name><Typ>TextBoxEinzeilig</Typ><Werte><Wert /><Vorbelegung /></Werte></Platzhalter></PlatzhalterListe></Stempelverfuegung>",
+                "<Stempelverfuegung><Id>00000000-0000-0000-0000-000000000003</Id><Name>teststempel3</Name><ErstellInformationenAnzeigen>False</ErstellInformationenAnzeigen><Farbe>#FF0000FF</Farbe><AufgabeErzeugen>False</AufgabeErzeugen><Beschreibung><PlatzhalterRef>1</PlatzhalterRef><br />Ab am <ZeitstempelRef /></Beschreibung><PlatzhalterListe><Platzhalter><Id>1</Id><Name>Texteingabe</Name><Typ>TextBoxEinzeilig</Typ><Werte><Wert /><Vorbelegung /></Werte></Platzhalter></PlatzhalterListe></Stempelverfuegung>",
+                "<Stempelverfuegung><Id>ed484460-bff4-4cd9-9a48-ace9759e9c1e</Id><Name>teststempel4</Name><ErstellInformationenAnzeigen>false</ErstellInformationenAnzeigen><Farbe /><AufgabeErzeugen>true</AufgabeErzeugen><Beschreibung><Beschreibung>test</Beschreibung></Beschreibung><PlatzhalterListe /><Funktionen /></Stempelverfuegung>"
+            };
         }
 
         public void speicherStempel(string stempel, Guid guid, string stempelName)
